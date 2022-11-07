22219e3964cc0ed1619d3d72a667cd142cbb333b
Sebastian Bejarano <slbejaranom@gmail.com>
2022-11-06 21:04:19 -0500

    First commit

---
+++ src/main/java/ingsoft/truequeandoback/TruequeandobackApplication.java
@@ -0,0 +1,13 @@
+package ingsoft.truequeandoback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TruequeandobackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TruequeandobackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ingsoft/truequeandoback/TruequeandobackApplicationTests.java
@@ -0,0 +1,13 @@
+package ingsoft.truequeandoback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TruequeandobackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
