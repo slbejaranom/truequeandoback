50e5f9a7523a68e4d199f836aeae207314218ce9
Ivan Villegas <idvillegasi@correo.udistrital.edu.co>
2022-11-16 22:07:28 -0500

    Se modifican las tablas con If not exist

--- src/main/resources/db/migration/tablas.sql
+++ src/main/resources/db/migration/tablas.sql
@@ -1,4 +1,4 @@
-CREATE TABLE usuarios (
+CREATE TABLE IF NOT EXISTS usuarios (
 	id_usuario integer PRIMARY KEY,
 	nombre_usuario varchar (100) NOT NULL,
 	email varchar (200) NOT NULL,
@@ -6,7 +6,7 @@ CREATE TABLE usuarios (
 	ubicacion varchar (100) NOT NULL
 	);
 	
-CREATE TABLE elementos (
+CREATE TABLE IF NOT EXISTS elementos (
 	id_elemento integer PRIMARY KEY,
 	nombre_elemento varchar(100) NOT NULL,
 	categoria integer NOT NULL,
@@ -20,25 +20,25 @@ CREATE TABLE elementos (
 );
 
 
-CREATE TABLE operadores_logisticos (
+CREATE TABLE IF NOT EXISTS operadores_logisticos (
 	id_operador_logistico integer PRIMARY KEY,
 	razon_social varchar (200) NOT NULL,
 	nit integer NOT NULL,
 	email varchar (200) NOT NULL,
-	contraseña varchar (50) NOT NULL
+	contraseña varchar (50) NOT NULL,
 	precioPorKilometro Real NOT NULL, 
 	precioPorKilogramo Real NOT NULL,
 	precioPorMetroCubico Real NOT NULL
 	);
 
-CREATE TABLE administradores (
+CREATE TABLE IF NOT EXISTS administradores (
 	id_administrador integer PRIMARY KEY,
 	nombre_administrador varchar (100) NOT NULL,
 	email varchar (200) NOT NULL,
 	contraseña varchar (50) NOT NULL
 	);
 	
-CREATE TABLE trueques (
+CREATE TABLE IF NOT EXISTS trueques (
 	id_trueque integer,
 	fecha_trueque date,
 	estado smallint NOT NULL,
@@ -50,7 +50,7 @@ CREATE TABLE trueques (
 	PRIMARY KEY (id_trueque, fecha_trueque)
 	);
 
-CREATE TABLE tokens (
+CREATE TABLE IF NOT EXISTS tokens (
 	id_token varchar(50) PRIMARY KEY,
 	timestampGeneracion varchar (50) NOT NULL,
 	expirationTime varchar (50) NOT NULL,
@@ -60,7 +60,7 @@ CREATE TABLE tokens (
 	id_operador_logistico integer REFERENCES operadores_logisticos (id_operador_logistico)
 	);
 	
-CREATE TABLE notificaciones_elemento (
+CREATE TABLE IF NOT EXISTS notificaciones_elemento (
 	id_notificacion_elemento integer PRIMARY KEY,
 	tipo boolean NOT NULL,
 	mensaje varchar (200) NOT NULL,
@@ -68,7 +68,7 @@ CREATE TABLE notificaciones_elemento (
 	id_elemento integer REFERENCES elementos (id_elemento) NOT NULL
 );
 
-CREATE TABLE notificaciones_trueque (
+CREATE TABLE IF NOT EXISTS notificaciones_trueque (
 	id_notificacion_trueque integer PRIMARY KEY,
 	id_administrador integer REFERENCES administradores (id_administrador)NOT NULL,
 	id_trueque integer NOT NULL,
