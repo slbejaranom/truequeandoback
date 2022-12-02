c727c2ad1f0d8335fc774e6ba977f288851d1466
Ivan Villegas <idvillegasi@correo.udistrital.edu.co>
2022-12-02 09:33:11 -0500

    Se cambian los nombres y se agregan campos en la base de datos

--- src/main/resources/db/migration/v1__crearBaseDatos.sql
+++ src/main/resources/db/migration/v1__crearBaseDatos.sql
@@ -1,14 +1,16 @@
 CREATE TABLE IF NOT EXISTS usuarios (
-	id_usuario integer PRIMARY KEY,
-	nombre_usuario varchar (100) NOT NULL,
-	email varchar (200) NOT NULL,
-	contraseña varchar (50) NOT NULL,
-	ubicacion varchar (100) NOT NULL
+	idUsuario integer PRIMARY KEY,
+	nombre varchar (100) NOT NULL,
+	email varchar (200) UNIQUE NOT NULL,
+	password varchar (50) NOT NULL,
+	direccion varchar (50) NOT NULL,
+	departamento varchar(50) NOT NULL,
+	municipio varchar(50) NOT NULL
 	);
 	
 CREATE TABLE IF NOT EXISTS elementos (
-	id_elemento integer PRIMARY KEY,
-	nombre_elemento varchar(100) NOT NULL,
+	idElemento integer PRIMARY KEY,
+	nombre varchar(100) NOT NULL,
 	categoria integer NOT NULL,
 	valor real NOT NULL,
 	estado boolean NOT NULL,
@@ -16,64 +18,64 @@ CREATE TABLE IF NOT EXISTS elementos (
 	alto real NOT NULL,
 	largo real NOT NULL,
 	ancho real NOT NULL,
-	id_usuario integer REFERENCES usuarios (id_usuario) NOT NULL
+	idUsuario integer REFERENCES usuarios (idUsuario) NOT NULL
 );
 
 
-CREATE TABLE IF NOT EXISTS operadores_logisticos (
-	id_operador_logistico integer PRIMARY KEY,
-	razon_social varchar (200) NOT NULL,
+CREATE TABLE IF NOT EXISTS operadoresLogisticos (
+	idOperadorLogistico integer PRIMARY KEY,
+	nombre varchar (200) NOT NULL,
 	nit integer NOT NULL,
-	email varchar (200) NOT NULL,
-	contraseña varchar (50) NOT NULL,
+	email varchar (200) UNIQUE NOT NULL,
+	password varchar (50) NOT NULL,
 	precioPorKilometro Real NOT NULL, 
 	precioPorKilogramo Real NOT NULL,
 	precioPorMetroCubico Real NOT NULL
 	);
 
 CREATE TABLE IF NOT EXISTS administradores (
-	id_administrador integer PRIMARY KEY,
-	nombre_administrador varchar (100) NOT NULL,
-	email varchar (200) NOT NULL,
-	contraseña varchar (50) NOT NULL
+	idAdministrador integer PRIMARY KEY,
+	nombre varchar (100) NOT NULL,
+	email varchar (200) UNIQUE NOT NULL,
+	password varchar (50) NOT NULL
 	);
 	
 CREATE TABLE IF NOT EXISTS trueques (
-	id_trueque integer,
-	fecha_trueque date,
+	idTrueque integer,
+	fechaTrueque date,
 	estado smallint NOT NULL,
-	id_usuario1 integer REFERENCES usuarios (id_usuario) NOT NULL, 
-	id_elemento1 integer REFERENCES elementos (id_elemento) NOT NULL,
-	id_usuario2 integer REFERENCES usuarios (id_usuario) NOT NULL,
-	id_elemento2 integer REFERENCES elementos (id_elemento) NOT NULL,
-	id_operador_logistico integer REFERENCES operadores_logisticos (id_operador_logistico) NOT NULL,
-	PRIMARY KEY (id_trueque, fecha_trueque)
+	idUsuario1 integer REFERENCES usuarios (idUsuario) NOT NULL, 
+	idElemento1 integer REFERENCES elementos (idElemento) NOT NULL,
+	idUsuario2 integer REFERENCES usuarios (idUsuario) NOT NULL,
+	idElemento2 integer REFERENCES elementos (idElemento) NOT NULL,
+	idOperadorLogistico integer REFERENCES operadoresLogisticos (idOperadorLogistico) NOT NULL,
+	PRIMARY KEY (idTrueque, fechaTrueque)
 	);
 
 CREATE TABLE IF NOT EXISTS tokens (
-	id_token varchar(50) PRIMARY KEY,
+	idToken varchar(50) PRIMARY KEY,
 	timestampGeneracion varchar (50) NOT NULL,
 	expirationTime varchar (50) NOT NULL,
 	active varchar (50) NOT NULL,
-	id_usuario integer REFERENCES usuarios (id_usuario),
-	id_administrador integer REFERENCES administradores (id_administrador),
-	id_operador_logistico integer REFERENCES operadores_logisticos (id_operador_logistico)
+	idUsuario integer REFERENCES usuarios (idUsuario),
+	idAdministrador integer REFERENCES administradores (idAdministrador),
+	idOperadorLogistico integer REFERENCES operadoresLogisticos (idOperadorLogistico)
 	);
 	
-CREATE TABLE IF NOT EXISTS notificaciones_elemento (
-	id_notificacion_elemento integer PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS notificacionesElemento (
+	idNotificacionElemento integer PRIMARY KEY,
 	tipo boolean NOT NULL,
 	mensaje varchar (200) NOT NULL,
-	id_administrador integer REFERENCES administradores (id_administrador)NOT NULL,
-	id_elemento integer REFERENCES elementos (id_elemento) NOT NULL
+	idAdministrador integer REFERENCES administradores (idAdministrador)NOT NULL,
+	idElemento integer REFERENCES elementos (idElemento) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS notificaciones_trueque (
-	id_notificacion_trueque integer PRIMARY KEY,
-	id_administrador integer REFERENCES administradores (id_administrador)NOT NULL,
-	id_trueque integer NOT NULL,
-	fecha_trueque date NOT NULL,
-	FOREIGN KEY (id_trueque, fecha_trueque) REFERENCES trueques (id_trueque, fecha_trueque)
+CREATE TABLE IF NOT EXISTS notificacionesTrueque (
+	idNotificacionTrueque integer PRIMARY KEY,
+	idAdministrador integer REFERENCES administradores (idAdministrador)NOT NULL,
+	idTrueque integer NOT NULL,
+	fechaTrueque date NOT NULL,
+	FOREIGN KEY (idTrueque,fechaTrueque) REFERENCES trueques (idTrueque,fechaTrueque)
 );
 	
 
