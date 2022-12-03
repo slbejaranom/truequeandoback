e8bb4eaac45e115bf9d8f7634e64a976d57afb82
Sebastian Bejarano <slbejaranom@gmail.com>
2022-12-03 18:05:47 -0500

    Added repository tags

--- src/main/java/ingsoft/truequeandoback/repository/AdministradorRepository.java
+++ src/main/java/ingsoft/truequeandoback/repository/AdministradorRepository.java
@@ -2,7 +2,9 @@ package ingsoft.truequeandoback.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import ingsoft.truequeandoback.domain.Administrador;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AdministradorRepository extends JpaRepository<Administrador,Long> {
 
 }
--- src/main/java/ingsoft/truequeandoback/repository/OperadorLogisticoRepository.java
+++ src/main/java/ingsoft/truequeandoback/repository/OperadorLogisticoRepository.java
@@ -2,7 +2,9 @@ package ingsoft.truequeandoback.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import ingsoft.truequeandoback.domain.OperadorLogistico;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OperadorLogisticoRepository extends JpaRepository<OperadorLogistico,Long> {
 
 }
