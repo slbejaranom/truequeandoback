d612da8dc58a414310666c113b70693d3b1b9bfd
Ivan Villegas <idvillegasi@correo.udistrital.edu.co>
2022-12-03 15:56:03 -0500

    se corrige la base de datos

--- src/main/resources/db/migration/v1__crearBaseDatos.sql
+++ src/main/resources/db/migration/v1__crearBaseDatos.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS usuarios (
 	departamento varchar(50) NOT NULL,
 	municipio varchar(50) NOT NULL
 	);
-	
+
 CREATE TABLE IF NOT EXISTS elementos (
 	idElemento integer PRIMARY KEY,
 	nombre varchar(100) NOT NULL,
@@ -39,17 +39,17 @@ CREATE TABLE IF NOT EXISTS administradores (
 	email varchar (200) UNIQUE NOT NULL,
 	password varchar (50) NOT NULL
 	);
-	
+
 CREATE TABLE IF NOT EXISTS trueques (
 	idTrueque integer,
-	fechaTrueque date,
+	fecha date,
 	estado smallint NOT NULL,
 	idUsuario1 integer REFERENCES usuarios (idUsuario) NOT NULL, 
 	idElemento1 integer REFERENCES elementos (idElemento) NOT NULL,
 	idUsuario2 integer REFERENCES usuarios (idUsuario) NOT NULL,
 	idElemento2 integer REFERENCES elementos (idElemento) NOT NULL,
 	idOperadorLogistico integer REFERENCES operadoresLogisticos (idOperadorLogistico) NOT NULL,
-	PRIMARY KEY (idTrueque, fechaTrueque)
+	PRIMARY KEY (idTrueque, fecha)
 	);
 
 CREATE TABLE IF NOT EXISTS tokens (
@@ -57,25 +57,23 @@ CREATE TABLE IF NOT EXISTS tokens (
 	timestampGeneracion varchar (50) NOT NULL,
 	expirationTime varchar (50) NOT NULL,
 	active varchar (50) NOT NULL,
+	valorToken text NOT NULL,
 	idUsuario integer REFERENCES usuarios (idUsuario),
 	idAdministrador integer REFERENCES administradores (idAdministrador),
 	idOperadorLogistico integer REFERENCES operadoresLogisticos (idOperadorLogistico)
 	);
-	
+
 CREATE TABLE IF NOT EXISTS notificacionesElemento (
 	idNotificacionElemento integer PRIMARY KEY,
 	tipo boolean NOT NULL,
+	fecha date NOT NULL,
 	mensaje varchar (200) NOT NULL,
-	idAdministrador integer REFERENCES administradores (idAdministrador)NOT NULL,
 	idElemento integer REFERENCES elementos (idElemento) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS notificacionesTrueque (
 	idNotificacionTrueque integer PRIMARY KEY,
-	idAdministrador integer REFERENCES administradores (idAdministrador)NOT NULL,
 	idTrueque integer NOT NULL,
-	fechaTrueque date NOT NULL,
-	FOREIGN KEY (idTrueque,fechaTrueque) REFERENCES trueques (idTrueque,fechaTrueque)
+	fecha date NOT NULL,
+	FOREIGN KEY (idTrueque,fecha) REFERENCES trueques (idTrueque,fecha)
 );
-	
-
