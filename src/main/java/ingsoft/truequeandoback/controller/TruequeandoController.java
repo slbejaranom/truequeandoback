822f3c8ababa9ab2bcb864a575c412ddeb78a8f1
IvanDanielVillegas <51217454+IvanDanielVillegas@users.noreply.github.com>
2022-12-04 21:17:09 -0500

    Features/servicio autenticacion AYYYUDAAAAA (#12)
    * Se crean el metodo autenticar dto y el generar tokendto
    
    * Se agrega variable nit a la clase administrador
    
    * Se agrega variable nit a la clase operador logistico, se elimina de administrador
    
    * Se agrega el metodo para registrar
    
    * se cambia el metodo get por find
    
    * Ahora si queda bien el metodo registrar, o eso espero
    
    * Login no funciona HELP
    
    * cambios en el aplication propeties
    
    * Todavia no sirve HELP
    
    * cambios en application properties
    
    borrar cambios en application properties
    
    Co-authored-by: slbejaranom <45303676+slbejaranom@users.noreply.github.com>

--- src/main/java/ingsoft/truequeandoback/controller/TruequeandoController.java
+++ src/main/java/ingsoft/truequeandoback/controller/TruequeandoController.java
@@ -5,6 +5,7 @@ import ingsoft.truequeandoback.domain.ServicioAutenticacion;
 import ingsoft.truequeandoback.domain.Usuario;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TruequeandoController {
     private final ServicioAutenticacion servicioAutenticacion;
     @PostMapping("/autenticar")
-    public AutenticacionDTO autenticar (Usuario usuario){
+    public AutenticacionDTO autenticar (@RequestBody Usuario usuario){
         return servicioAutenticacion.autenticar(usuario.getEmail(), usuario.getPassword());
     }
 }
