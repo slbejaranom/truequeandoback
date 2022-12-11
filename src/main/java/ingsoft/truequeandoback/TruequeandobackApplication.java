8f1bb2f8bff77119680fcd0f2f5b86439b9069ed
Sebastian Bejarano <slbejaranom@gmail.com>
2022-12-11 01:09:13 -0500

    Fixed cors again

--- src/main/java/ingsoft/truequeandoback/TruequeandobackApplication.java
+++ src/main/java/ingsoft/truequeandoback/TruequeandobackApplication.java
@@ -3,7 +3,7 @@ package ingsoft.truequeandoback;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication()
 public class TruequeandobackApplication {
 
 	public static void main(String[] args) {
